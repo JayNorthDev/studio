2a3bc2182202de91b0dd1a2c7753ab6fafefc609
Jay North <jaynorthinfo@gmail.com>
2026-03-24 20:55:19 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4096233960-5684a",
+  "appId": "1:760017880182:web:f151fdf9f7f4a6be0a4b57",
+  "apiKey": "AIzaSyALyRtrb2s11NsTLnojnWZnyTJz-yuHGlk",
+  "authDomain": "studio-4096233960-5684a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "760017880182"
+};
